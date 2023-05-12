eafea04cb7cd55bf400832bd127dafa330a35f94
1G000 <gorbatovskiy.igor@gmail.com>
2023-05-12 20:31:08 +0300

    add photos and data.js

---
+++ scripts/data.js
@@ -0,0 +1,28 @@
+const rackets = [
+  {
+    modelName: "Babolat Pure Strike 16/19",
+    imgUrl: "../img/babolat-pure-strike-16-19.jpg",
+    alt: "Babolat Pure Strike 16/19 photo",
+    date: "12.05.2023",
+  },
+  {
+    modelName: "Babolat Pure Strike Team",
+    imgUrl: "../img/Babolat Pure Strike Team.jpg",
+    date: "13.05.2023",
+  },
+]
+
+// Babolat Pure Strike Team;
+// Head Gravity Lite 2021;
+// Babolat Pure Aero;
+// Babolat Pure Aero Rafa Team 27
+// Babolat Pure Aero Rafa
+// Head Radical PRO 27
+// Head Speed MP 2022
+// Wilson Blade 98
+// Tecnifibre Tfight 305 RS
+// Tecnifibre Tempo 298
+// Wilson Blade 100 L
+// Tecnifibre Tfight 305 Isoflex
+
+export default rackets
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,16 @@
+const rackets = [
+  {
+    modelName: "Babolat Pure Strike 16/19",
+    imgUrl: "../img/babolat-pure-strike-16-19.jpg",
+    date: "12.05.2023",
+  },
+  {
+    modelName: "Babolat Pure Strike Team",
+    imgUrl: "../img/Babolat Pure Strike Team.jpg",
+    date: "13.05.2023",
+  },
+]
+
 const buyBtn = document.querySelector("#buy-btn")
 const closeCartBtn = document.querySelector("#close-cart")
 const buyItem = document.querySelector("#buy-item")
