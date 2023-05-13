e7d7665b3fa99c5cb122fa7313740017d496644f
1G000 <gorbatovskiy.igor@gmail.com>
2023-05-13 16:01:50 +0300

    displayed card elements on the page and added modules

--- scripts/data.js
+++ scripts/data.js
@@ -1,28 +1,81 @@
 const rackets = [
   {
     modelName: "Babolat Pure Strike 16/19",
-    imgUrl: "../img/babolat-pure-strike-16-19.jpg",
-    alt: "Babolat Pure Strike 16/19 photo",
+    imgUrl: "./img/Babolat Pure Strike 16 19.jpg",
+    alt: "Babolat Pure Strike 16/19 img",
     date: "12.05.2023",
   },
   {
     modelName: "Babolat Pure Strike Team",
-    imgUrl: "../img/Babolat Pure Strike Team.jpg",
+    imgUrl: "./img/Babolat Pure Strike Team.jpg",
+    alt: "Babolat Pure Strike Team img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Head Gravity Lite 2021",
+    imgUrl: "./img/Head Gravity Lite 2021.jpg",
+    alt: "Head Gravity Lite 2021 img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Babolat Pure Aero",
+    imgUrl: "./img/Babolat Pure Aero.jpg",
+    alt: "Babolat Pure Aero img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Babolat Pure Aero Rafa Team 27",
+    imgUrl: "./img/Babolat Pure Aero Rafa Team 27.jpg",
+    alt: "Babolat Pure Aero Rafa Team 27 img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Babolat Pure Aero Rafa",
+    imgUrl: "./img/Babolat Pure Aero Rafa.jpg",
+    alt: "Babolat Pure Aero Rafa img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Head Radical PRO 27",
+    imgUrl: "./img/Head Radical PRO 27.jpg",
+    alt: "Head Radical PRO 27 img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Head Speed MP 2022",
+    imgUrl: "./img/Head Speed MP 2022.jpg",
+    alt: "Head Speed MP 2022 img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Wilson Blade 98",
+    imgUrl: "./img/Wilson Blade 98.jpg",
+    alt: "Wilson Blade 98 img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Tecnifibre Tfight 305 RS",
+    imgUrl: "./img/Tecnifibre Tfight 305 RS.jpg",
+    alt: "Tecnifibre Tfight 305 RS img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Tecnifibre Tempo 298",
+    imgUrl: "./img/Tecnifibre Tempo 298.jpg",
+    alt: "Tecnifibre Tempo 298 img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Wilson Blade 100 L",
+    imgUrl: "./img/Wilson Blade 100L.jpg",
+    alt: "Wilson Blade 100 L img",
+    date: "13.05.2023",
+  },
+  {
+    modelName: "Tecnifibre Tfight 305 Isoflex",
+    imgUrl: "./img/Tecnifibre Tfight 305 Isoflex.jpg",
+    alt: "Tecnifibre Tfight 305 Isoflex img",
     date: "13.05.2023",
   },
 ]
-
-// Babolat Pure Strike Team;
-// Head Gravity Lite 2021;
-// Babolat Pure Aero;
-// Babolat Pure Aero Rafa Team 27
-// Babolat Pure Aero Rafa
-// Head Radical PRO 27
-// Head Speed MP 2022
-// Wilson Blade 98
-// Tecnifibre Tfight 305 RS
-// Tecnifibre Tempo 298
-// Wilson Blade 100 L
-// Tecnifibre Tfight 305 Isoflex
-
 export default rackets
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,47 @@
-const rackets = [
-  {
-    modelName: "Babolat Pure Strike 16/19",
-    imgUrl: "../img/babolat-pure-strike-16-19.jpg",
-    date: "12.05.2023",
-  },
-  {
-    modelName: "Babolat Pure Strike Team",
-    imgUrl: "../img/Babolat Pure Strike Team.jpg",
-    date: "13.05.2023",
-  },
-]
+import rackets from "./data.js"
 
-const buyBtn = document.querySelector("#buy-btn")
+function addSection() {
+  const section = document.createElement("div")
+  section.classList.add("items")
+  const wrapper = document.querySelector(".rackets")
+  wrapper.insertBefore(section, wrapper.children[1])
+}
+addSection()
+
+Object.values(rackets).map((el) => {
+  const card = document.createElement("div")
+  card.classList.add("item")
+
+  const cards = document.querySelector(".items")
+  cards.insertBefore(card, cards.children[1])
+
+  const itemImg = document.createElement("img")
+  itemImg.classList.add("item-img")
+  itemImg.src = `${el.imgUrl}`
+  itemImg.alt = `${el.alt}`
+  card.insertBefore(itemImg, card.children[1])
+
+  const itemName = document.createElement("h2")
+  itemName.classList.add("item-name")
+  itemName.innerHTML += `${el.modelName}`
+  card.insertBefore(itemName, card.children[2])
+
+  const itemDate = document.createElement("p")
+  itemDate.classList.add("item-date")
+  card.insertBefore(itemDate, card.children[3])
+  itemDate.innerHTML += `${el.date}`
+
+  const buyBtn = document.createElement("button")
+  buyBtn.classList.add("buy-btn")
+  buyBtn.addEventListener("click", function () {
+    document.querySelector("#my-modal").classList.add("open")
+  })
+  card.insertBefore(buyBtn, card.children[4])
+  buyBtn.innerText += `Купить`
+})
+
 const closeCartBtn = document.querySelector("#close-cart")
 const buyItem = document.querySelector("#buy-item")
-
-buyBtn.addEventListener("click", function () {
-  document.querySelector("#my-modal").classList.add("open")
-})
 
 closeCartBtn.addEventListener("click", function () {
   document.querySelector("#my-modal").classList.remove("open")
