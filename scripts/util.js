200bce5664280abb44ffaf052e6c7802ae5037a1
1G000 <gorbatovskiy.igor@gmail.com>
2023-05-27 23:42:45 +0300

    fixed getDayInfo func and ready to check

--- scripts/util.js
+++ scripts/util.js
@@ -34,19 +34,19 @@ export function getDayInfo(t) {
   let firstDay = new Date(`${yyyy}-${mm}-01`).getDay()
   let weekOfMonth
   if (firstDay == 2) {
-    weekOfMonth = Math.floor((date + 1) / 7) + 1
+    weekOfMonth = Math.ceil((date + 1) / 7)
   } else if (firstDay == 3) {
-    weekOfMonth = Math.floor((date + 2) / 7) + 1
+    weekOfMonth = Math.ceil((date + 2) / 7)
   } else if (firstDay == 4) {
-    weekOfMonth = Math.floor((date + 3) / 7) + 1
+    weekOfMonth = Math.ceil((date + 3) / 7)
   } else if (firstDay == 5) {
-    weekOfMonth = Math.floor((date + 4) / 7) + 1
+    weekOfMonth = Math.ceil((date + 4) / 7)
   } else if (firstDay == 6) {
-    weekOfMonth = Math.floor((date + 5) / 7) + 1
+    weekOfMonth = Math.ceil((date + 5) / 7)
   } else if (firstDay == 0) {
-    weekOfMonth = Math.floor((date + 6) / 7) + 1
+    weekOfMonth = Math.ceil((date + 6) / 7)
   } else {
-    weekOfMonth = Math.floor(date / 7) + 1
+    weekOfMonth = Math.ceil(date / 7)
   }
 
   return `${dayName}, ${weekOfMonth} неделя ${month} ${year} года `
